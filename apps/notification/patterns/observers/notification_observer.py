73198f6cc4f4e55466cdc8446285a7a24553da28
Josephine Alvina L <113569179+Jsphinlvina@users.noreply.github.com>
2026-05-13 15:00:37 +0700

    Revert "Merge Week 1 Joshua Subianto Setup dan skeleton untuk notifikasi"

--- apps/notification/patterns/observers/notification_observer.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class NotificationObserver(ABC):
-    @abstractmethod
-    def on_notification(self, event, data):
-        ...
--- core/permissions.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-
-class AuthenticatedPlaceholder(BasePermission):
-    def has_permission(self, request, view):
-        return True
