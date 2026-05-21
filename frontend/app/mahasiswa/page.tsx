8e61271d4cbbda692159206b3cbefd689862a41d
Josephine Alvina L <2272029@maranatha.ac.id>
2026-05-21 16:40:14 +0700

    Menghubungkan login dengan front-end

---
+++ frontend/app/mahasiswa/page.tsx
@@ -0,0 +1,9 @@
+export default function DashboardMahasiswa() {
+    return (
+        <div className="p-8">
+            <h1 className="text-2xl font-bold text-[#355872]">
+                Selamat Datang di Dashboard Mahasiswa 🎓
+            </h1>
+        </div>
+    );
+}
